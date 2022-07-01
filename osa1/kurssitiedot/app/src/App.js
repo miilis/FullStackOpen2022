92d324362a606dff3a0ed358b7668cfc08869d7f
Toni Miilunpalo <you@example.com>
2022-07-01 13:07:52 +0300

    Add Tehtävä 1.4

--- osa1/kurssitiedot/app/src/App.js
+++ osa1/kurssitiedot/app/src/App.js
@@ -28,7 +28,7 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.num_exercises}</p>
+      <p>Number of exercises {sum_exercises(props.parts)}</p>
     </>
   )
 }
@@ -43,25 +43,26 @@ const sum_exercises = (parts) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const parts = [part1, part2, part3]
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
       <Content parts={parts} />
-      <Total num_exercises={sum_exercises(parts)} />
+      <Total parts={parts} />
     </div>
   )
 }
