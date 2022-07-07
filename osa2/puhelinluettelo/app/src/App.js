8f308de245941f732486cd7b2bbb4bf1f2bc9ae1
Toni Miilunpalo <you@example.com>
2022-07-07 17:31:42 +0300

    Add Tehtävä 2.9

--- osa2/puhelinluettelo/app/src/App.js
+++ osa2/puhelinluettelo/app/src/App.js
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 
-const Persons = ({persons}) => {
+const Person = ({person}) => {
+  return (
+    <p>{person.name} {person.number}</p>
+  )
+}
+
+const Persons = ({persons, filter}) => {
+  if (filter) {
+    const personsFiltered = persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+    persons = personsFiltered
+  }
+  
   return (
     <div>
       {persons.map((person) =>
-        <p key={person.name}>{person.name} {person.number}</p>
+        <Person key={person.name} person={person}/>
       )}
     </div>
   )
@@ -12,10 +23,14 @@ const Persons = ({persons}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleNewNameChange = (event) => {
     // console.log(event.target.value);
@@ -25,6 +40,11 @@ const App = () => {
   const handleNewNumberChange = (event) => {
     // console.log(event.target.value);
     setNewNumber(event.target.value)    
+  }
+
+  const handleNewFilterChange = (event) => {
+    // console.log(event.target.value);
+    setNewFilter(event.target.value)    
   }
   
   const addNewName = (event) => {
@@ -50,6 +70,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter show with: <input value={newFilter} onChange={handleNewFilterChange} />
+      </div>
+      <h2>add new</h2>
       <form onSubmit={addNewName}>
         <div>
           name: <input value={newName} onChange={handleNewNameChange} />
@@ -62,7 +86,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons}/>
+      <Persons persons={persons} filter={newFilter}/>
     </div>
   )
 
