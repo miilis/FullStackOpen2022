37ac11c86850e34dcc3ccc54df1e932a4d8ba485
Toni Miilunpalo <you@example.com>
2022-07-07 12:55:44 +0300

    Add Tehtävä 2.6

---
+++ osa2/puhelinluettelo/app/src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const Persons = ({persons}) => {
+  return (
+    <div>
+      {persons.map((person) =>
+        <p key={person.name}>{person.name}</p>
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNewNameChange = (event) => {
+    // console.log(event.target.value);
+    setNewName(event.target.value)    
+  }
+  
+  const addNewName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: <input value={newName} onChange={handleNewNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Persons persons={persons}/>
+    </div>
+  )
+
+}
+
+export default App
